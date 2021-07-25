bb4f0dee25014062ccb01e018a641abf54cbdda1
Filip Š <filip.stamcar@hotmail.com>
2021-07-25 11:20:45 +0200

    Add support for changing install locations using build-time environment variables (closes #17)

--- native/src/directories.rs
+++ native/src/directories.rs
@@ -1,9 +1,22 @@
 use std::fs::create_dir_all;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Result};
 use cfg_if::cfg_if;
 use directories::BaseDirs;
+
+fn expand_tilde<P: AsRef<str>, H: AsRef<Path>>(path: P, home: H) -> PathBuf {
+    let path = path.as_ref();
+    let home = home.as_ref();
+
+    // Path does not contain tilde, just return it
+    if !path.starts_with('~') {
+        return PathBuf::from(path);
+    }
+
+    // Join home directory with the path
+    home.join(&path[2..])
+}
 
 #[non_exhaustive]
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -16,6 +29,9 @@ pub struct ProjectDirs {
     /// native messaging connections from the browser extension, native
     /// app manifest, and the shell completions.
     ///
+    /// Can be overwritten by a `FFPWA_EXECUTABLES` build-time environment variable.
+    ///
+    /// Default value:
     /// - Windows: `C:\Program Files\FirefoxPWA\` or `C:\Program Files (x86)\FirefoxPWA\`
     /// - Linux: `/usr/bin/`
     /// - macOS: `/usr/local/bin/`
@@ -27,6 +43,9 @@ pub struct ProjectDirs {
     /// Stores the UserChrome modifications which are later copied to
     /// the user-specific profile directories at the site-launch-time.
     ///
+    /// Can be overwritten by a `FFPWA_SYSDATA` build-time environment variable.
+    ///
+    /// Default value:
     /// - Windows: `C:\Program Files\FirefoxPWA\` or `C:\Program Files (x86)\FirefoxPWA\`
     /// - Linux: `/usr/share/firefoxpwa/`
     /// - macOS: `/usr/local/share/firefoxpwa/`
@@ -38,6 +57,9 @@ pub struct ProjectDirs {
     /// Stores the installed browser runtime, profile directories with
     /// user data, site icons (on Windows), as well as config and log files.
     ///
+    /// Can be overwritten by a `FFPWA_USERDATA` build-time environment variable.
+    ///
+    /// Default value:
     /// - Windows: `%APPDATA%\FirefoxPWA\`
     /// - Linux: `$XDG_DATA_HOME/firefoxpwa/` or `$HOME/.local/share/firefoxpwa/`
     /// - macOS: `$HOME/Library/Application Support/firefoxpwa/`
@@ -47,6 +69,13 @@ pub struct ProjectDirs {
 
 impl ProjectDirs {
     pub fn new() -> Result<Self> {
+        // We need base directories to get the user directory (for expanding tilde)
+        // and the app data directory (for a default user data location)
+        let base = BaseDirs::new().context("Failed to determine base system directories")?;
+
+        // On Windows, executables and system data are in the same directory
+        // We can just obtain it once, store it, and re-use it for both directories
+        // Variable needs to be prefixed with `_` so clippy does not complain on other OSes
         let _install = {
             cfg_if! {
                 if #[cfg(target_os = "windows")] {
@@ -60,7 +89,9 @@ impl ProjectDirs {
             }
         };
 
-        let executables = {
+        let executables = if let Some(envvar) = option_env!("FFPWA_EXECUTABLES") {
+            expand_tilde(envvar, base.home_dir())
+        } else {
             cfg_if! {
                 if #[cfg(target_os = "windows")] {
                     _install.clone()
@@ -74,7 +105,9 @@ impl ProjectDirs {
             }
         };
 
-        let sysdata = {
+        let sysdata = if let Some(envvar) = option_env!("FFPWA_SYSDATA") {
+            expand_tilde(envvar, base.home_dir())
+        } else {
             cfg_if! {
                 if #[cfg(target_os = "windows")] {
                     _install
@@ -88,23 +121,19 @@ impl ProjectDirs {
             }
         };
 
-        let userdata = {
-            let base = BaseDirs::new().context("Failed to determine base system directories")?;
-            let appdata = base.data_dir();
-
+        let userdata = if let Some(envvar) = option_env!("FFPWA_USERDATA") {
+            expand_tilde(envvar, base.home_dir())
+        } else {
             cfg_if! {
-                if #[cfg(any(target_os = "linux", target_os = "macos"))] { appdata.join("firefoxpwa") }
-                else { appdata.join("FirefoxPWA") }
+                if #[cfg(any(target_os = "linux", target_os = "macos"))] { base.data_dir().join("firefoxpwa") }
+                else { base.data_dir().join("FirefoxPWA") }
             }
         };
 
-        // If you want to overwrite install locations, do this here
+        // If you want to overwrite install locations, use build-time environment variables
         // See the struct fields comments for description about each directory
-        // See the commented lines below for a simple example
-        // let executables = PathBuf::from("EXECUTABLES_DIR");
-        // let sysdata = PathBuf::from("SYSDATA_DIR");
-        // let userdata = PathBuf::from("USERDATA_DIR");
 
+        // If you really need to overwrite install locations by editing source code, do this here
         // You can rely on the below line as an injection target for tools such as `sed`
         // INSTALL_LOCATIONS_INJECTION
 
